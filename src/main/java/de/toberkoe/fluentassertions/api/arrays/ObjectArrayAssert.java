c34f3c3bfdd2b3c5e523d8dcb4f9c8818addbc5b
t.bertram-koehler <t.bertramkoehler@gmail.com>
2018-03-12 21:29:39 +0100

    array assertions
    closes #28

--- src/main/java/de/toberkoe/fluentassertions/api/arrays/ObjectArrayAssert.java
+++ src/main/java/de/toberkoe/fluentassertions/api/arrays/ObjectArrayAssert.java
@@ -1,5 +1,13 @@
 package de.toberkoe.fluentassertions.api.arrays;
 
+import de.toberkoe.fluentassertions.api.Assertions;
+
+/**
+ * {@code Object[]} Assertions.
+ * Create a new instance by using {@link Assertions#assertThat(Object[])}.
+ *
+ * @author t.bertram-koehler
+ */
 public class ObjectArrayAssert extends AbstractArrayAssert<ObjectArrayAssert, Object> {
 
     public ObjectArrayAssert(Object[] value) {
