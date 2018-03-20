7a63c3a2a159ddd36f8ed2774c7c69c0540b0837
t.bertram-koehler <t.bertramkoehler@gmail.com>
2018-03-20 09:30:12 +0100

    private constructor to hide implicit public constructor

--- src/main/java/de/toberkoe/fluentassertions/internal/DateConverter.java
+++ src/main/java/de/toberkoe/fluentassertions/internal/DateConverter.java
@@ -5,7 +5,10 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 
-public class DateConverter {
+public final class DateConverter {
+
+    private DateConverter() {
+    }
 
     public static LocalDate toLocalDate(Date date) {
         return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
